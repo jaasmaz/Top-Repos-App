3e2349e418b954e1972220ae4c81eb00718aba01
jaasmaz <jaasmaz@yahoo.com>
2021-10-24 15:45:34 -0700

    Clean up unused code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,28 +29,6 @@ export default {
   },
   data: () => ({
     name: "Home",
-    apiData: null,
-    formData: null,
-    selectedLang: null,
-    repoCount: null,
-    dropLang: ["Javascript", "Dart", "PHP", "python", "Java"],
   }),
-  computed: {
-    fetchAPI() {
-      return this.$store.getters.getformData;
-    },
-  },
-  created() {},
-  // methods: {
-  //   async getRepos() {
-  //     this.$refs.form.validate();
-  //     if (this.selectedLang && this.repoCount && this.$refs.form.validate()) {
-  //       const api = await axios.get(
-  //         `https://api.github.com/search/repositories?q=language:${this.selectedLang}`
-  //       );
-  //       this.apiData = api.data.items.slice(0, this.repoCount);
-  //     }
-  //   },
-  // },
 };
 </script>
